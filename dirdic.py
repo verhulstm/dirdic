2f76f80c98fca975c5baccaef99aa5860f7782e7
verhulstm <michael@terminallabs.com>
2020-04-18 16:36:22 -0500

    added in sample dir

--- dirdic.py
+++ dirdic.py
@@ -1,26 +1,41 @@
 import os
 
-def __walk(path):
-    folders = []
-    files = []
-    # r=root, d=directories, f = files
-    for r, d, f in os.walk(path):
-        for folder in d:
-            folders.append(os.path.join(r, folder))
-        for file in f:
-            files.append(os.path.join(r, file))
-    return (folders, files)
-
 class Dirdict:
-    def swim(self):
-        print("The shark is swimming.")
+    def __init__(self, path):
+        self.path = os.path.abspath(path)
 
-    def be_awesome(self):
-        print("The shark is being awesome.")
+    def __walk(self, path):
+        folders = []
+        files = []
+        for r, d, f in os.walk(path):
+            for folder in d:
+                folders.append(os.path.abspath(os.path.join(r, folder)))
+            for file in f:
+                files.append(os.path.abspath(os.path.join(r, file)))
+        return (sorted(folders), sorted(files))
 
-def dir2dict(path):
-    shark = Dirdict()
-    shark.swim()
-    #print(__walk(path))
+    def allfilepaths(self):
+        print(self.__walk(self.path)[1])
 
-dir2dict("path")
+    def alldirpaths(self):
+        print(self.__walk(self.path)[0])
+
+    def allfilenames(self):
+        filenames = []
+        for p in self.__walk(self.path)[1]:
+            filename=os.path.basename(p)
+            filenames.append(filename)
+        print(filenames)
+
+    def alldirnames(self):
+        dirnames = []
+        for d in self.__walk(self.path)[0]:
+            dirname=os.path.basename(d)
+            dirnames.append(dirname)
+        print(dirnames)
+
+data = Dirdict("sample")
+data.allfilepaths()
+data.alldirpaths()
+data.allfilenames()
+data.alldirnames()
